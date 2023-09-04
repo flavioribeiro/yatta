beaf125120c53c6ed40689bfa85b3d3b4efd7613
Flávio Ribeiro <email@flavioribeiro.com>
2023-09-04 12:49:28 -0400

    utils: patch codec mime for av1 given that gst is not exposing profile/level properly

--- src/utils.rs
+++ src/utils.rs
@@ -10,10 +10,18 @@ pub(crate) fn probe_encoder(state: Arc<Mutex<State>>, enc: gst::Element) {
         move |_pad, info| match info.data {
             Some(gst::PadProbeData::Event(ref ev)) => match ev.view() {
                 gst::EventView::Caps(e) => {
-                    let mime = gst_pbutils::codec_utils_caps_get_mime_codec(e.caps());
+                    let mut mime = gst_pbutils::codec_utils_caps_get_mime_codec(e.caps()).unwrap();
+                    let structure = e.caps().structure(0).unwrap();
+                    if structure.name() == "video/x-av1" {
+                        // https://www.reddit.com/r/AV1/comments/stbk3r/av1_in_hls_manifests_works_for_browsers_that/
+                        // https://github.com/GStreamer/gst-plugins-base/blob/master/gst-libs/gst/pbutils/codec-utils.c#L2402-L2404
+                        // https://aomediacodec.github.io/av1-isobmff/#codecsparam
+                        // TODO parse the codec_data from structure and set the mime accordingly
+                        mime = "av01.0.00M.08".into();
+                    }
 
                     let mut state = state.lock().unwrap();
-                    state.all_mimes.push(mime.unwrap().into());
+                    state.all_mimes.push(mime.into());
                     state.try_write_manifest();
                     gst::PadProbeReturn::Remove
                 }
